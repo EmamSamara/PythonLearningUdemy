044193b629c539bc1a1fa988915186a0803975bc
Emam Samara <137582218+EmamSamara@users.noreply.github.com>
2024-10-25 04:44:19 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+
+my_integer = 10   #ما بأثر شو تسمي المتغير بس حطلو اي اسم وحط القيمة الي بدك ياها 
+my_float = 3.14
+my_string = 'emam'
+my_boolean=True
+
+print(type(my_integer))   #print the type of the variable
+print(type(my_float))
+print(type(my_string))
+print(type(my_boolean))
+
+print(my_integer)
+print(my_boolean)
+print(my_string)
+print(my_boolean)
+
